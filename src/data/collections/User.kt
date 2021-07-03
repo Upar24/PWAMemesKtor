73619bce6209f438b92e343ed4968a9bee77c7a0
Upar24 <alfionita.fina24@gmail.com>
2021-07-03 15:34:11 +0700

    fix image init

--- src/data/collections/User.kt
+++ src/data/collections/User.kt
@@ -8,7 +8,7 @@ data class User(
     val password: String,
     val following: List<String> = listOf(),
     val followers: List<String> = listOf(),
-    val image: String = "R.drawable.image",
+    val image: String = "R.drawable.image0",
     val bio: String = "",
     val score:Int= 1000,
     @BsonId
